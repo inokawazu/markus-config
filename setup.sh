d7d16d74ad6e0c17d0c8916a25b400de8350db6f
Markus Amano <markus.a.amano@gmail.com>
2021-01-09 22:00:33 -0600

    added a check for plugged dir

--- setup.sh
+++ setup.sh
@@ -4,8 +4,12 @@ echo "This is my setup script, it is designed to install all of my, Markus Amano
 # Downloads the vim-plug
 # curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Makes the vim plug dir
-mkdir ~/.vim/plugged
+# Makes the vim plug dir if it does not exist.
+PLUGGED_DIR=~/.vim/plugged
+
+if [-f $PLUGGED_DIR]; then
+	mkdir $PLUGGED_DIR
+fi
 
 #Downloads the .vimrc file
 rm ~/.vimrc

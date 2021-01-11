ef6a849c08229310caa397b46172ccde49b3e164
Markus Amano <markus.a.amano@gmail.com>
2021-01-10 21:14:18 -0600

    added silence to curl commands

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo "This is my setup script, it is designed to install all of my, Markus Amano's, presets."
 
-# Downloads the vim-plug
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 # Makes the vim plug dir if it does not exist.
 PLUGGED_DIR=~/.vim/plugged
 
@@ -15,9 +12,9 @@ fi
 mkdir -p ~/.vim/colors
 
 # Downloads the themes for vim.
-curl -fLo ~/.vim/colors/jellybeans.vim https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
+curl -sfLo ~/.vim/colors/jellybeans.vim https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
 
 
 #Downloads the .vimrc file
 rm ~/.vimrc
-curl -fLo ~/.vimrc https://raw.githubusercontent.com/mgarbiso/markus-config/main/.vimrc
+curl -sfLo ~/.vimrc https://raw.githubusercontent.com/mgarbiso/markus-config/main/.vimrc

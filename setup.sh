77a516d7d61350b2ac5bcc83c2a91f958765c309
Markus Garbiso <markus.a.amano@gmail.com>
2021-03-16 20:14:24 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "This is my setup script, it is designed to install all of my, Markus Amano's, presets."
+echo "This is my setup script, it is designed to install all of my presets."
 
 # Makes the vim plug dir if it does not exist.
 mkdir -p ~/.vim/plugged
@@ -10,11 +10,15 @@ mkdir -p ~/.vim/colors
 # Downloads the themes for vim.
 curl -sfLo ~/.vim/colors/jellybeans.vim https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
 
-source /usr/share/defaults/etc/profile
-PATH=$HOME/.local/lib/python3.7/site-packages:$PATH; export PATH
+echo "INSTALLED: colors"
+
+# source /usr/share/defaults/etc/profile
+# PATH=$HOME/.local/lib/python3.7/site-packages:$PATH; export PATH
 
 #Downloads the .vimrc file
 if [ -f "~/.vimrc" ]; then
     rm "~/.vimrc"
 fi
 curl -sfLo ~/.vimrc https://raw.githubusercontent.com/mgarbiso/markus-config/main/.vimrc
+
+echo "INSTALLED: .vimrc"

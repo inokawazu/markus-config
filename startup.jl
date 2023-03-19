92bcbbd1fe275ead5caba86d225c4785c243a2b2
Markus Amano <markus.a.amano@gmail.com>
2023-03-19 14:58:51 +0800

    made a template

--- startup.jl
+++ startup.jl
@@ -3,3 +3,10 @@ using Revise
 using CodeTracking
 
 enable_autocomplete_brackets(false)
+
+function mytemplate()
+    @eval begin
+        using PkgTemplates
+        Template(dir = ".", user = "inokawazu")
+    end
+end

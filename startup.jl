b6fbeb0f401d1c7b9a01b7cba20a658d4a71669a
Markus-Amano <markus.a.amano@gmail.com>
2023-04-15 21:07:13 +0800

    repl stuff only starts with a REPL present

--- startup.jl
+++ startup.jl
@@ -1,12 +1,11 @@
-using OhMyREPL
-using Revise
-using CodeTracking
-
-enable_autocomplete_brackets(false)
-
-function mytemplate()
-    @eval begin
-        using PkgTemplates
-        Template(dir = ".", user = "inokawazu")
+atreplinit() do repl
+    try
+        @eval begin 
+            using Revise
+            using OhMyREPL
+            enable_autocomplete_brackets(false)
+        end
+    catch e
+        @warn "error while importing OhMyREPL" e
     end
 end

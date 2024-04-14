86849840b11a98c72f584f6dfff2dbaba7776775
Markus Amano <markus.a.amano@gmail.com>
2024-04-14 16:01:13 +0900

    moved nvim config

--- env_variables.zsh
+++ env_variables.zsh
@@ -8,8 +8,6 @@ export GIT_AUTHOR_EMAIL="$EMAIL"
 export GIT_COMMITTER_NAME="$NAME"
 export GIT_COMMITTER_EMAIL="$EMAIL"
 
-export DISTRO="$(cat /etc/*-release | grep '^ID=' | cut -d'=' -f2)"
-
 export EDITOR=""
 if is_command nvim
 then 
--- setup.zsh
+++ setup.zsh
@@ -31,8 +31,9 @@ if read -q "reply?Would you like to set up zsh files? (y/n): "
 then
     echo
     if read -q "reply?Would you like to set up antigen? (y/n): "
+    then
         curl -sfL git.io/antigen > ~/.antigen.zsh
-    end
+    fi
 
     echo
     for zsh_file in antigen.zsh aliases.zsh env_variables.zsh key_bindings.zsh
@@ -45,6 +46,9 @@ echo
 
 # sourcing vim
 install_source_file vim_settings.vim ~/.vimrc "source %s"
+
+# sourcing nvim
+install_source_file nvim/init.lua ~/.config/nvim/init.lua 'dofile("%s")'
 
 #Add tmux config file
 install_source_file tmux.conf ~/.tmux.conf "source %s"

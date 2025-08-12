10f64125cddf9902c9f49e18f677a78f10dd4dea
Markus Amano <markus.a.amano@gmail.com>
2025-08-12 15:19:07 +0900

    removed Revise from default packages

--- startup.jl
+++ startup.jl
@@ -1,8 +1,8 @@
 using Pkg: Pkg # Hack for Julia 1.11 release: https://github.com/KristofferC/OhMyREPL.jl/issues/345
 atreplinit() do repl
-    @eval begin 
-        using Revise
-        using OhMyREPL
-        enable_autocomplete_brackets(false)
+    try
+        @eval using OhMyREPL
+    catch e
+        @warn "error while importing OhMyREPL" e
     end
 end

ba0120c459abdb10a48993ee44f2c433714784d9
Markus Amano <markus.a.amano@gmail.com>
2026-02-24 21:27:11 +0900

    removed autocomplete

--- startup.jl
+++ startup.jl
@@ -1,7 +1,10 @@
 using Pkg: Pkg # Hack for Julia 1.11 release: https://github.com/KristofferC/OhMyREPL.jl/issues/345
 atreplinit() do repl
     try
-        @eval using OhMyREPL
+        @eval begin 
+            using OhMyREPL
+            enable_autocomplete_brackets(false)
+        end
     catch e
         @warn "error while importing OhMyREPL" e
     end

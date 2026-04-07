65cfee901b753a158bc9f5cc72f0a13f33499a82
Markus Amano <markus.a.amano@gmail.com>
2026-04-07 21:38:54 +0900

    updated themes and bundles

--- antigen.zsh
+++ antigen.zsh
@@ -13,13 +13,12 @@ antigen bundle common-aliases
 antigen bundle qrcode
 
 # external plugins
-antigen bundle MichaelAquilina/zsh-you-should-use
+# antigen bundle MichaelAquilina/zsh-you-should-use
 antigen bundle unixorn/autoupdate-antigen.zshplugin
-# antigen bundle zsh-users/zsh-autosuggestions
+
+# themes
+antigen theme nebirhos
 
 antigen bundle zsh-users/zsh-syntax-highlighting # must be last plugin to get effect
-
-# theme
-antigen theme gianu
 
 antigen apply
